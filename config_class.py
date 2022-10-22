473c7e8e35110a970071a77382be44f073faa242
Edward J. Parkinson <saultyevil@gmail.com>
2022-10-22 21:57:58 +0300

    fix scope issue with class variables

--- config_class.py
+++ config_class.py
@@ -24,6 +24,7 @@ class App:
 
     # __conf is a dictionary of configuration parameters
     __conf = {
+        "BOT_TOKEN": os.getenv("BOT_TOKEN"),
         # cooldown parameters
         "COOLDOWN_RATE": 3,
         "COOLDOWN_STANDARD": 60,
@@ -70,38 +71,8 @@ class App:
     __conf["NO_COOL_DOWN_USERS"] = [__conf["ID_USER_SAULTYEVIL"]]
     __conf["ALL_FILES"] = [__conf["USERS_FILE"], __conf["REMINDERS_FILE"], __conf["BANK_FILE"]]
 
-    @staticmethod
-    def __read_in_json_file(filepath: Path, conf_key: str) -> None:
-        """Read in a JSON file and set it to a __conf key.
-
-        Parameters
-        ----------
-        filepath: Path
-            The filepath to the file.
-        conf_key: str
-            The key for the file in the App.__conf dict.
-        """
-        with open(filepath, "r", encoding="utf-8") as file_in:
-            App.__conf[conf_key] = json.load(file_in)
-
-    __read_in_json_file(__conf["USERS_FILE"], "USERS_FILE_STREAM")
-    __read_in_json_file(__conf["REMINDERS_FILE"], "REMINDERs_FILE_STREAM")
-    __read_in_json_file(__conf["BANK_FILE"], "BANK_FILE_STREAM")
-
-    @staticmethod
-    def __on_directory_change(_):
-        App.__read_in_json_file(App.__conf["USERS_FILE"], "USERS_FILE_STREAM")
-        App.__read_in_json_file(App.__conf["REMINDERS_FILE"], "REMINDERs_FILE_STREAM")
-        App.__read_in_json_file(App.__conf["BANK_FILE"], "BANK_FILE_STREAM")
-
-    __observer = Observer()
-    __event_handler = PatternMatchingEventHandler(["*.json"], None, False, True)
-    __event_handler.on_modified = __on_directory_change
-    __observer.schedule(__event_handler, "./data", False)
-    __observer.start()
-
     # __setters is a tuple of parameters which can be set
-    __setters = ()
+    __setters = ("USERS_FILE_STREAM", "REMINDERS_FILE_STREAM", "BANK_FILE_STREAM")
 
     # Public methods -----------------------------------------------------------
 
@@ -130,4 +101,36 @@ class App:
         if name in App.__setters:
             App.__conf[name] = value
         else:
-            raise NameError("Name not accepted in set() method")
+            raise NameError(f"Name {name} not accepted in set() method")
+
+
+def __read_in_json_file(filepath: Path, conf_key: str) -> None:
+    """Read in a JSON file and set it to a __conf key.
+
+    Parameters
+    ----------
+    filepath: Path
+        The filepath to the file.
+    conf_key: str
+        The key for the file in the App.__conf dict.
+    """
+    with open(filepath, "r", encoding="utf-8") as file_in:
+        App.set(conf_key, json.load(file_in))
+
+
+__read_in_json_file(App.config("USERS_FILE"), "USERS_FILE_STREAM")
+__read_in_json_file(App.config("REMINDERS_FILE"), "REMINDERS_FILE_STREAM")
+__read_in_json_file(App.config("BANK_FILE"), "BANK_FILE_STREAM")
+
+
+def __on_directory_change(_):
+    __read_in_json_file(App.config("USERS_FILE"), "USERS_FILE_STREAM")
+    __read_in_json_file(App.config("REMINDERS_FILE"), "REMINDERS_FILE_STREAM")
+    __read_in_json_file(App.config("BANK_FILE"), "BANK_FILE_STREAM")
+
+
+__observer = Observer()
+__event_handler = PatternMatchingEventHandler(["*.json"], None, False, True)
+__event_handler.on_modified = __on_directory_change
+__observer.schedule(__event_handler, "./data", False)
+__observer.start()
