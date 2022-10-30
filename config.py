89cb9666a8a4800bb009db7f519d1bf5c7788660
Edward J. Parkinson <saultyevil@gmail.com>
2022-10-30 11:03:46 +0000

    add formatter to file logger

--- config.py
+++ config.py
@@ -129,11 +129,15 @@ logger = logging.getLogger(App.config("LOGGER_NAME"))
 formatter = logging.Formatter(
     "[%(asctime)s] %(levelname)8s : %(message)s (%(filename)s:%(lineno)d)", "%Y-%m-%d %H:%M:%S"
 )
+
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
+
 file_handler = RotatingFileHandler(
     filename=App.config("LOGFILE_NAME"), encoding="utf-8", maxBytes=int(5e5), backupCount=5
 )
+file_handler.setFormatter(formatter)
+
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
