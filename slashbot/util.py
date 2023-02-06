39114f741cb4d8c9a4e934b498508a8c68be0628
Edward J. Parkinson <saultyevil@gmail.com>
2023-02-06 19:28:31 +0000

    [refactor,documentation] util changes

--- slashbot/util.py
+++ slashbot/util.py
@@ -7,12 +7,12 @@ from typing import Any
 import disnake
 
 
-def convert_string_to_lower(_inter: disnake.ApplicationCommandInteraction, variable: Any):
+def convert_string_to_lower(_inter: disnake.ApplicationCommandInteraction, variable: Any) -> Any:
     """Slash command convertor to transform a string into all lower case.
 
     Parameters
     ----------
-    -inter : disnake.ApplicationCommandInteraction
+    _inter : disnake.ApplicationCommandInteraction
         The slash command interaction. Currently unused.
     variable : Any
         The possible string to convert into lower case.
@@ -24,3 +24,21 @@ def convert_string_to_lower(_inter: disnake.ApplicationCommandInteraction, varia
         Otherwise the original variable is returned.
     """
     return variable.lower() if isinstance(variable, str) else variable
+
+
+def convert_yes_no_to_bool(_inter: disnake.ApplicationCommandInteraction, choice: str) -> bool:
+    """_summary_
+
+    Parameters
+    ----------
+    _inter : disnake.ApplicationCommandInteraction
+        The slash command interaction. Currently unused.
+    choice : str
+        The yes/no string to convert into a bool.
+
+    Returns
+    -------
+    bool
+        True or False depending on yes or no.
+    """
+    return True if choice.lower() == "yes" else False
