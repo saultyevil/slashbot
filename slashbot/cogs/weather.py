843e80d7a4453726cb962ab4341b7e3542f60a1e
Edward J. Parkinson <saultyevil@gmail.com>
2023-05-07 13:50:26 +0100

    [feat] improve safety and error tracking for one call api requests

--- slashbot/cogs/weather.py
+++ slashbot/cogs/weather.py
@@ -3,7 +3,6 @@
 
 """Commands for getting the weather."""
 
-import datetime
 import json
 import logging
 from types import coroutine
@@ -28,8 +27,20 @@ logger = logging.getLogger(App.config("LOGGER_NAME"))
 
 COOLDOWN_USER = commands.BucketType.user
 WEATHER_UNITS = ["metric", "imperial"]
-WEATHER_COMMAND_CHOICES = ["everything", "temperature", "humidity", "rain", "wind"]
+FORECAST_TYPES = ["hourly", "daily"]
 API_KEY = App.config("OWM_API_KEY")
+
+
+class GeocodeException(Exception):
+    """Geocoding API failure"""
+
+
+class OneCallException(Exception):
+    """OneCall API failure"""
+
+
+class LocationNotFoundException(Exception):
+    """Location not in OWM failure"""
 
 
 class WeatherCommands(CustomCog):
@@ -70,7 +81,7 @@ class WeatherCommands(CustomCog):
         -------
         The cardinal direction as a string.
         """
-        dirs = [
+        directions = [
             "N",
             "NNE",
             "NE",
@@ -88,10 +99,11 @@ class WeatherCommands(CustomCog):
             "NW",
             "NNW",
         ]
-        idx = round(degrees / (360.0 / len(dirs)))
-        return dirs[idx % 16]
 
-    def __get_user_city(self, user_id: str, user_name: str) -> str:
+        return directions[round(degrees / (360.0 / len(directions))) % len(directions)]
+
+    @staticmethod
+    def __get_user_location(user_id: str, user_name: str) -> str:
         """Return the stored location set by a user.
 
         Parameters
@@ -111,7 +123,8 @@ class WeatherCommands(CustomCog):
 
             return user.city
 
-    def __get_weather_response(self, location: str, units: str) -> Tuple[str, dict]:
+    @staticmethod
+    def __get_weather_response(location: str, units: str, extract_type: str) -> Tuple[str, dict]:
         """_summary_
 
         Parameters
@@ -119,6 +132,8 @@ class WeatherCommands(CustomCog):
         location : str
             _description_
         units : str
+            _description_
+        extract_type : str
             _description_
 
         Returns
@@ -139,9 +154,14 @@ class WeatherCommands(CustomCog):
         )
 
         if geocode_request.status_code != 200:
-            raise requests.RequestException(f"Geocoding API failed for {location}")
+            raise GeocodeException(f"Geocoding API failed for {location}")
 
-        geocode = json.loads(geocode_request.content)[0]
+        geocode = json.loads(geocode_request.content)
+
+        if len(geocode) == 0:
+            raise LocationNotFoundException(f"{location} not found in Geocoding API")
+
+        geocode = geocode[0]
         lat, lon = geocode["lat"], geocode["lon"]
         name, country = geocode["name"], geocode["country"]
 
@@ -151,9 +171,12 @@ class WeatherCommands(CustomCog):
         )
 
         if one_call_request.status_code != 200:
-            raise requests.RequestException(f"OneCall API failed for {location}")
+            if one_call_request.status_code == 400:
+                raise LocationNotFoundException(f"{location} could not be found")
+            else:
+                raise OneCallException(f"OneCall API failed for {location}")
 
-        return f"{name}, {country}", json.loads(one_call_request.content)
+        return f"{name}, {country}", json.loads(one_call_request.content)[extract_type]
 
     @staticmethod
     def __get_weather_icon_url(icon_code: str) -> str:
@@ -171,35 +194,6 @@ class WeatherCommands(CustomCog):
         """
         return f"https://openweathermap.org/img/wn/{icon_code}@2x.png"
 
-    def __get_units_for_system(self, system: str) -> dict:
-        """Get the units for the system.
-
-        Parameters
-        ----------
-        system: str
-            The system of units to use, either metric or imperial.
-
-        Returns
-        -------
-        units: dict
-            The units in use, with keys t_units, t_units_fmt, w_units,
-            w_units_fmt.
-        """
-        if system == "imperial":
-            return {
-                "t_units": "fahrenheit",
-                "t_units_fmt": "F",
-                "w_units": "miles_hour",
-                "w_units_fmt": "mph",
-            }
-
-        return {
-            "t_units": "celsius",
-            "t_units_fmt": "C",
-            "w_units": "meters_sec",
-            "w_units_fmt": "km/h",
-        }
-
     # Commands -----------------------------------------------------------------
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
@@ -207,13 +201,16 @@ class WeatherCommands(CustomCog):
     async def forecast(  # pylint: disable=too-many-locals
         self,
         inter: disnake.ApplicationCommandInteraction,
-        city: str = commands.Param(
-            description="The city to get weather at, default is your saved location.", default=None
+        user_location: str = commands.Param(
+            name="location", description="The city to get weather at, default is your saved location.", default=None
+        ),
+        forecast_type: str = commands.Param(
+            description="The type of forecast to return.", default="daily", choices=FORECAST_TYPES
         ),
         units: str = commands.Param(
             description="The units to return weather readings in.", default="metric", choices=WEATHER_UNITS
         ),
-        days: int = commands.Param(description="The number of days to get the weather for.", default=4, gt=0, lt=8),
+        amount: int = commands.Param(description="The number of results to return.", default=3, gt=0, lt=5),
     ) -> coroutine:
         """Print the weather forecast for a location.
 
@@ -228,25 +225,38 @@ class WeatherCommands(CustomCog):
         """
         await inter.response.defer()
 
-        if not city:
-            city = self.__get_user_city(inter.author.id, inter.author.name)
-            if not city:
+        if not user_location:
+            user_location = self.__get_user_location(inter.author.id, inter.author.name)
+            if not user_location:
                 return await deferred_error_message(
                     inter, "You need to either specify a city, or set your city and/or country using /set_info."
                 )
 
-        location, weather = self.__get_weather_response(city, units)
-        weather = weather["current"]
+        try:
+            location, forecast = self.__get_weather_response(user_location, units, forecast_type)
+        except (LocationNotFoundException, GeocodeException):
+            return await deferred_error_message(inter, f"{user_location} is not available in OpenWeatherMap.")
+        except OneCallException:
+            return await deferred_error_message(inter, "OpenWeatherMap OneCall API has returned an error.")
+        except requests.Timeout:
+            return await deferred_error_message(inter, "OpenWeatherMap API has timed out.")
 
-        embed = disnake.Embed(title=f"Weather for {location}", color=disnake.Color.default())
+        embed = disnake.Embed(
+            title=f"{forecast_type.capitalize()} forecast for {location}", color=disnake.Color.default()
+        )
+
+        embed.set_footer(text=f"{self.get_generated_sentence('forecast')}")
+        embed.set_thumbnail(self.__get_weather_icon_url(forecast["weather"][0]["icon"]))
+
+        return await inter.edit_original_message(embed=embed)
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
     @commands.slash_command(name="weather", description="get the current weather")
     async def weather(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        city: str = commands.Param(
-            description="The city to get weather for, default is your saved location.", default=None
+        user_location: str = commands.Param(
+            name="location", description="The city to get weather for, default is your saved location.", default=None
         ),
         units: str = commands.Param(
             description="The units to return weather readings in.", default="metric", choices=WEATHER_UNITS
@@ -267,15 +277,21 @@ class WeatherCommands(CustomCog):
         """
         await inter.response.defer()
 
-        if not city:
-            city = self.__get_user_city(inter.author.id, inter.author.name)
-            if not city:
+        if not user_location:
+            user_location = self.__get_user_location(inter.author.id, inter.author.name)
+            if not user_location:
                 return await deferred_error_message(
                     inter, "You need to specify a city, or set your city and/or country using /set_info."
                 )
 
-        location, weather = self.__get_weather_response(city, units)
-        weather = weather["current"]
+        try:
+            location, weather = self.__get_weather_response(user_location, units, "current")
+        except (LocationNotFoundException, GeocodeException):
+            return await deferred_error_message(inter, f"{user_location} is not available in OpenWeatherMap.")
+        except OneCallException:
+            return await deferred_error_message(inter, "OpenWeatherMap OneCall API has returned an error.")
+        except requests.Timeout:
+            return await deferred_error_message(inter, "OpenWeatherMap API has timed out.")
 
         if units == "metric":
             temp_unit, wind_unit, wind_factor = "C", "km/h", 3.6
