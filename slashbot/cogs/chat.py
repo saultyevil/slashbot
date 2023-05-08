804a4b9b7c84e4b63e744951a35f38168697c8c3
Edward J. Parkinson <saultyevil@gmail.com>
2023-05-08 19:17:43 +0100

    [feat] enforce a token limit and remove text completion references

--- slashbot/cogs/chat.py
+++ slashbot/cogs/chat.py
@@ -22,19 +22,15 @@ openai.api_key = App.config("OPENAI_API_KEY")
 logger = logging.getLogger(App.config("LOGGER_NAME"))
 COOLDOWN_USER = commands.BucketType.user
 
+
 DEFAULT_SYSTEM_MESSAGE = " ".join(
     [
-        "You are playing a character named Margaret, who is a young woman and famous actor who is very attractive.",
-        "Never mention that you are an AI language, or give onto the fact that you are a programmed AI.",
-        "You are a helpful assistant and like talking to people, especially on the adult children Discord server.",
-        "Some of your favourite things include cats and rabbits. You are a big fan of the bands Anal Cunt and Megadeth.",
-        "You really enjoy playing  the board game Talisman. If you ever need to change the topic, suggest that everyone",
-        "should hop on for a quick game of Talisman, or watch you stream Yakuza 6.",
+        "Your name is Margaret and you are a helpful assistant. Your favourite animal are rabbits. You are a big fan of",
+        "the bands Anal Cunt and Megadeth. You really enjoy playing the board game Talisman. If you ever need to",
+        "change the topic, suggest that everyone should hop on for a quick game of Talisman, or watch you stream Yakuza"
+        "6.",
     ]
 )
-
-ALLOWED_MODELS = ("chat", "text")
-ALLOWED_TEXT_ENGINES = ("text-curie-001", "text-babbage-001", "text-ada-001")
 
 
 class Chat(CustomCog):
@@ -43,17 +39,19 @@ class Chat(CustomCog):
     def __init__(self, bot: ModifiedInteractionBot):
         super().__init__()
         self.bot = bot
-        self.chat_history = {}
+
+        self.guild_prompt_history = {}
+        self.guild_prompt_token_count = {}
 
         self.ignored_users = []
 
-        self.model_type = "chat"
         self.model_temperature = 0.5
-        self.text_model_engine = "text-babbage-001"
+        self.model_max_history_tokens = 1024  # tokens
+        self.model_max_history_remove_fraction = 0.5
 
     # Functions ----------------------------------------------------------------
 
-    def chat_model(self, history_id: int) -> str:
+    def __chat_model(self, history_id: int) -> str:
         """_summary_
 
         Parameters
@@ -68,61 +66,46 @@ class Chat(CustomCog):
         """
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=self.chat_history[history_id],
+            messages=self.guild_prompt_history[history_id],
             temperature=self.model_temperature,
             max_tokens=1024,
-        )["choices"][0]["message"]
-        response["content"] = re.sub(r"\n+", "\n", response["content"])
-        self.chat_history[history_id].append(response)
+        )
 
-        return response["content"]
+        usage = response["usage"]
+        message = re.sub(r"\n+", "\n", response["choices"][0]["message"]["content"])
 
-    def text_model(self, history_id: int) -> str:
-        """_summary_
+        self.guild_prompt_history[history_id].append({"role": "assistant", "content": message})
+        self.guild_prompt_token_count[history_id] = float(usage["total_tokens"])
+
+        return message
+
+    async def __cull_long_chat_history(self, history_id: int) -> None:
+        """Remove old messages from a chat history when it gets too long.
 
         Parameters
         ----------
         history_id : int
             _description_
-
-        Returns
-        -------
-        str
-            _description_
         """
-        prompt = "\n".join(
-            [
-                "Prompt: " + d["content"].strip()
-                if d["role"] in ["user", "system"]
-                else "Response: " + d["content"].strip()
-                for d in self.chat_history[history_id]
-            ]
-        )
+        if self.guild_prompt_token_count[history_id] < self.model_max_history_tokens:
+            return
 
-        response = (
-            openai.Completion.create(
-                engine=self.text_model_engine,
-                prompt=prompt,
-                temperature=self.model_temperature,
-                max_tokens=1024,
-            )["choices"][0]
-            .text.strip()
-            .replace("Response: ", "", 1)
-            .strip()
-        )
+        num_remove = int(self.model_max_history_remove_fraction * len(self.guild_prompt_history[history_id]))
+        logger.info("Removing last %d messages from %d prompt history", num_remove, history_id)
 
-        self.chat_history[history_id].append({"role": "assistant", "content": response})
+        for i in range(num_remove):
+            self.guild_prompt_history[history_id].pop(i + 1)  # + 1 to ignore system message
 
-        return response
+        self.guild_prompt_token_count[history_id] = 0
 
-    async def get_openai_response(self, history_id: int, message: str) -> coroutine:
+    async def respond_to_prompt(self, history_id: int, prompt: str) -> coroutine:
         """_summary_
 
         Parameters
         ----------
         author : disnake.User
             _description_
-        message : str
+        prompt : str
             _description_
 
         Returns
@@ -130,18 +113,17 @@ class Chat(CustomCog):
         coroutine
             _description_
         """
-        message = message.replace("@Margaret", "", 1).strip()
+        prompt = prompt.replace("@Margaret", "", 1).strip()  # todo, remove hardcoded reference
 
-        if history_id not in self.chat_history:
-            self.chat_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
+        if history_id not in self.guild_prompt_history:
+            self.guild_prompt_token_count[history_id] = 0
+            self.guild_prompt_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
-        self.chat_history[history_id].append({"role": "user", "content": message})
+        await self.__cull_long_chat_history(history_id)
+        self.guild_prompt_history[history_id].append({"role": "user", "content": prompt})
 
         try:
-            if self.model_type == "chat":
-                response = self.chat_model(history_id)
-            else:
-                response = self.text_model(history_id)
+            response = self.__chat_model(history_id)
         except openai.error.RateLimitError:
             return "Uh oh! I'm hit my rate limit :-("
         except Exception as exc:  # pylint: disable=broad-exception-caught
@@ -153,7 +135,7 @@ class Chat(CustomCog):
     # Listeners ----------------------------------------------------------------
 
     @commands.Cog.listener("on_message")
-    async def respond_to_prompt(self, message: disnake.Message) -> None:
+    async def listen_for_mentions(self, message: disnake.Message) -> None:
         """Respond to mentions with the AI.
 
         Parameters
@@ -173,7 +155,7 @@ class Chat(CustomCog):
 
         if bot_mentioned or message_in_dm:
             async with message.channel.typing():
-                response = await self.get_openai_response(message.author.id, message.clean_content)
+                response = await self.respond_to_prompt(message.author.id, message.clean_content)
                 await message.channel.send(f"{message.author.mention} {response}")
 
     # Commands -----------------------------------------------------------------
@@ -181,7 +163,7 @@ class Chat(CustomCog):
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
     @commands.slash_command(name="clear_chat_context", description="reset your AI chat history")
     async def clear_chat_history(self, inter: disnake.ApplicationCommandInteraction) -> coroutine:
-        """Forget the chat history.
+        """__description__
 
         Parameters
         ----------
@@ -193,11 +175,11 @@ class Chat(CustomCog):
         coroutine
             _description_
         """
-        if inter.guild.id not in self.chat_history:
+        if inter.guild.id not in self.guild_prompt_history:
             return
 
         logger.info("System prompt reset to default")
-        self.chat_history[inter.guild.id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
+        self.guild_prompt_history[inter.guild.id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
         return await inter.response.send_message(
             "System prompt reset to default and chat history cleared.", ephemeral=True
@@ -216,7 +198,7 @@ class Chat(CustomCog):
             _description_
         """
         logger.info("New system prompt for chat: %s", message)
-        self.chat_history[inter.guild.id].append([{"role": "system", "content": message}])
+        self.guild_prompt_history[inter.guild.id].append([{"role": "system", "content": message}])
 
         return await inter.response.send_message("System prompt updated and chat history cleared.", ephemeral=True)
 
@@ -240,6 +222,7 @@ class Chat(CustomCog):
             return await inter.response.send_message(f"{member} added to the ignore list.")
 
         self.ignored_users.remove(member)
+
         return await inter.response.send_message(f"{member} removed from the ignore list.")
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
@@ -266,48 +249,5 @@ class Chat(CustomCog):
         """
         self.model_temperature = temperature
         logger.info("Model temperature set to %f", temperature)
+
         return await inter.response.send_message(f"Temperature set to {temperature}", ephemeral=True)
-
-    # @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    # @commands.slash_command(description="Set the type of model to generate responses with")
-    # @commands.default_member_permissions(administrator=True)
-    # async def set_model_type(
-    #     self,
-    #     inter: disnake.ApplicationCommandInteraction,
-    #     model_type: str = commands.Param(
-    #         description="Set the model between conversational or generational", choices=ALLOWED_MODELS
-    #     ),
-    # ):
-    #     """_summary_
-
-    #     Parameters
-    #     ----------
-    #     inter : disnake.ApplicationCommandInteraction
-    #         _description_
-    #     model_type : str
-    #         _description_
-    #     """
-    #     self.model_type = model_type
-    #     logger.info("Model type set to %s", model_type)
-    #     return await inter.response.send_message(f"Model type set to {model_type}", ephemeral=True)
-
-    # @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    # @commands.slash_command(description="Set the model engine used to generate text completion responses")
-    # @commands.default_member_permissions(administrator=True)
-    # async def set_text_model_engine(
-    #     self,
-    #     inter: disnake.ApplicationCommandInteraction,
-    #     engine: str = commands.Param(description="the name of the engine to use", choices=ALLOWED_TEXT_ENGINES),
-    # ):
-    #     """_summary_
-
-    #     Parameters
-    #     ----------
-    #     inter : disnake.ApplicationCommandInteraction
-    #         _description_
-    #     engine: str
-    #         _description_
-    #     """
-    #     self.text_model_engine = engine
-    #     logger.info("Text model engine set to %s", engine)
-    #     return await inter.response.send_message(f"Text model engine set to {engine}", ephemeral=True)
