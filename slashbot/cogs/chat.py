1ab9cb9ac451daf2cdb9451dde68c28a507e3e28
Edward J. Parkinson <saultyevil@gmail.com>
2023-05-11 21:16:24 +0100

    [feat] add rate limiting for ai responses

--- slashbot/cogs/chat.py
+++ slashbot/cogs/chat.py
@@ -4,8 +4,10 @@
 """Commands for interacting with the OpenAI chat bot interface."""
 
 import logging
+import time
 import re
 from types import coroutine
+from collections import defaultdict
 
 import openai
 import openai.error
@@ -32,6 +34,11 @@ DEFAULT_SYSTEM_MESSAGE = " ".join(
     ]
 )
 
+TIME_LIMITED_SERVERS = [
+    App.config("ID_SERVER_ADULT_CHILDREN"),
+    App.config("ID_SERVER_FREEDOM"),
+]
+
 
 class Chat(CustomCog):
     """Chat tools for the bot."""
@@ -41,9 +48,8 @@ class Chat(CustomCog):
         self.bot = bot
 
         self.guild_prompt_history = {}
-        self.guild_prompt_token_count = {}
-
-        self.ignored_users = []
+        self.guild_prompt_token_count = defaultdict(dict)
+        self.guild_cooldown = defaultdict(dict)
 
         self.model_temperature = 0.5
         self.model_max_history_tokens = 1024  # tokens
@@ -51,7 +57,7 @@ class Chat(CustomCog):
 
     # Functions ----------------------------------------------------------------
 
-    def __chat_model(self, history_id: int) -> str:
+    def __openai_chat_completion(self, history_id: int) -> str:
         """_summary_
 
         Parameters
@@ -79,7 +85,7 @@ class Chat(CustomCog):
 
         return message
 
-    async def __cull_long_chat_history(self, history_id: int) -> None:
+    async def __trim_message_history(self, history_id: int) -> None:
         """Remove old messages from a chat history when it gets too long.
 
         Parameters
@@ -119,18 +125,35 @@ class Chat(CustomCog):
             self.guild_prompt_token_count[history_id] = 0
             self.guild_prompt_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
-        await self.__cull_long_chat_history(history_id)
+        await self.__trim_message_history(history_id)
         self.guild_prompt_history[history_id].append({"role": "user", "content": prompt})
 
         try:
-            response = self.__chat_model(history_id)
+            response = self.__openai_chat_completion(history_id)
         except openai.error.RateLimitError:
-            return "Uh oh! I'm hit my rate limit :-("
+            return "Uh oh! I've hit OpenAI's rate limit :-("
         except Exception as exc:  # pylint: disable=broad-exception-caught
             logger.error("OpenAI API failed with exception %s", exc)
             return "Uh oh! Something went wrong with that request :-("
 
         return response
+
+    def __get_cooldown_length(self, guild_id, author) -> int:
+        """_summary_
+
+        Parameters
+        ----------
+        guild_id : _type_
+            _description_
+        author : _type_
+            _description_
+
+        Returns
+        -------
+        int
+            _description_
+        """
+        return App.config("COOLDOWN_STANDARD"), App.config("COOLDOWN_RATE")
 
     # Listeners ----------------------------------------------------------------
 
@@ -143,25 +166,43 @@ class Chat(CustomCog):
         message : str
             _description_
         """
-        if message.author.bot:
-            return
-        if message.author == App.config("BOT_USER_OBJECT"):
-            return
-        if message.author in self.ignored_users:
+        # ignore other both messages and itself
+        if message.author.bot or message.author == App.config("BOT_USER_OBJECT"):
             return
 
+        # only respond when mentioned or in DMs
         bot_mentioned = App.config("BOT_USER_OBJECT") in message.mentions
         message_in_dm = isinstance(message.channel, disnake.channel.DMChannel)
 
         if bot_mentioned or message_in_dm:
+            # first check for any time limiting
+            if message.guild.id in TIME_LIMITED_SERVERS:
+                current_time = time.time()
+                last_message_time, message_count = self.guild_cooldown[message.guild.id].get(message.author.id, (0, 0))
+                elapsed_time = current_time - last_message_time
+                cooldown_length, max_message_count = self.__get_cooldown_length(message.guild.id, message.author)
+                if elapsed_time <= cooldown_length and message_count >= max_message_count:
+                    try:
+                        await message.delete(delay=10)
+                        return await message.channel.send(f"Stop abusing me {message.author.mention}!", delete_after=10)
+                    except disnake.Forbidden:
+                        logger.error("Bot does not have permission to delete time limited message.")
+                    return
+                if message_count >= cooldown_length:
+                    message_count = 0
+                message_count += 1
+                self.guild_cooldown[message.guild.id][message.author.id] = (current_time, message_count)
+
+            # if everything ok, type and send
             async with message.channel.typing():
-                response = await self.respond_to_prompt(message.author.id, message.clean_content)
-                await message.channel.send(f"{message.author.mention} {response}")
+                response = await self.respond_to_prompt(message.guild.id, message.clean_content)
+
+            await message.channel.send(f"{message.author.mention} {response}")
 
     # Commands -----------------------------------------------------------------
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(name="clear_chat_context", description="reset your AI chat history")
+    @commands.slash_command(name="clear_ai_chat_history", description="reset your AI chat history")
     async def clear_chat_history(self, inter: disnake.ApplicationCommandInteraction) -> coroutine:
         """__description__
 
@@ -176,9 +217,9 @@ class Chat(CustomCog):
             _description_
         """
         if inter.guild.id not in self.guild_prompt_history:
-            return
+            return await inter.response.send_message("There is no chat history to clear.", ephemeral=True)
 
-        logger.info("System prompt reset to default")
+        logger.info("System prompt reset to default for %s", inter.guild.name)
         self.guild_prompt_history[inter.guild.id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
         return await inter.response.send_message(
@@ -197,57 +238,10 @@ class Chat(CustomCog):
         message : str
             _description_
         """
-        logger.info("New system prompt for chat: %s", message)
-        self.guild_prompt_history[inter.guild.id].append([{"role": "system", "content": message}])
+        if inter.guild.id in self.guild_prompt_history:
+            self.guild_prompt_history[inter.guild.id].append([{"role": "system", "content": message}])
+        else:
+            self.guild_prompt_history[inter.guild.id] = [{"role": "system", "content": message}]
+        logger.info("New system prompt for chat %s: %s", inter.guild.name, message)
 
         return await inter.response.send_message("System prompt updated and chat history cleared.", ephemeral=True)
-
-    @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(description="Toggle a user in the ignore list")
-    @commands.default_member_permissions(administrator=True)
-    async def toggle_user_in_ignore_list(
-        self,
-        inter: disnake.ApplicationCommandInteraction,
-        member: disnake.Member = commands.Param(description="the user to toggle from the ignore list"),
-    ):
-        """Add or remove a user from the ignore list.
-
-        Parameters
-        ----------
-        inter : disnake.ApplicationCommandInteraction
-            _description_
-        """
-        if member not in self.ignored_users:
-            self.ignored_users.append(member)
-            return await inter.response.send_message(f"{member} added to the ignore list.")
-
-        self.ignored_users.remove(member)
-
-        return await inter.response.send_message(f"{member} removed from the ignore list.")
-
-    @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(description="Set the randomness of generated responses")
-    @commands.default_member_permissions(administrator=True)
-    async def set_model_temperature(
-        self,
-        inter: disnake.ApplicationCommandInteraction,
-        temperature: float = commands.Param(
-            description="larger values result in more random responses",
-            default=0.5,
-            ge=0,
-            le=2,
-        ),
-    ):
-        """_summary_
-
-        Parameters
-        ----------
-        inter : disnake.ApplicationCommandInteraction
-            _description_
-        temperature : float
-            _description_
-        """
-        self.model_temperature = temperature
-        logger.info("Model temperature set to %f", temperature)
-
-        return await inter.response.send_message(f"Temperature set to {temperature}", ephemeral=True)
