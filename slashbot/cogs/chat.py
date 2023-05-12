5a1003045dc06baf8580462173289b5f7701ecb5
Edward J. Parkinson <saultyevil@gmail.com>
2023-05-12 08:50:04 +0100

    [tweak] increase max message count

--- slashbot/cogs/chat.py
+++ slashbot/cogs/chat.py
@@ -158,7 +158,7 @@ class Chat(CustomCog):
         """
         if guild_id == App.config("ID_SERVER_ADULT_CHILDREN"):
             if App.config("ID_ROLE_TOP_GAY") in [role.id for role in author.roles]:
-                return (0, 0)
+                return (0, 999)
             return (App.config("COOLDOWN_STANDARD"), App.config("COOLDOWN_RATE"))
 
         return App.config("COOLDOWN_STANDARD"), App.config("COOLDOWN_RATE")
