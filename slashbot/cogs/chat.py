ecea04720eb059758e6dff73b433c7aba6f33aaf
Edward J. Parkinson <saultyevil@gmail.com>
2023-05-16 20:18:35 +0100

    [bugfix] address a lot of issues ;-)

--- slashbot/cogs/chat.py
+++ slashbot/cogs/chat.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """Cog for AI interactions, from the OpenAI API."""
+
 import copy
 import logging
 import time
@@ -10,7 +11,6 @@ from collections import defaultdict
 from types import coroutine
 from typing import Tuple
 
-import nltk
 import disnake
 import openai
 import openai.error
@@ -25,18 +25,13 @@ openai.api_key = App.config("OPENAI_API_KEY")
 logger = logging.getLogger(App.config("LOGGER_NAME"))
 COOLDOWN_USER = commands.BucketType.user
 
-# nltk.download("punkt")
-
 
 DEFAULT_SYSTEM_MESSAGE = " ".join(
     [
-        "You are role playing as Margaret, an ignorant conspiracy theorist who swears a lot and is an abusing",
+        "You are role playing as Margaret, an ignorant Reddit user who swears a lot and is an abusing",
         "personality. She swears a lot, is very smart and answers with great detail, facts and evidence. You must",
         "always be concise and your responses should not be longer than a few sentences. Play a story where you are",
         "having a conversation with a friend and answering questions.",
-        # "Your name is Margaret. You must follow these guidelines when writing your responses."
-        # "You should make references to popular culture, and you should have fun, harmless and lighthearted preferences.",
-        # "You must ALWAYS be concise. Your responses should be fewer than a couple of sentences.",
     ]
 )
 
@@ -45,6 +40,8 @@ TIME_LIMITED_SERVERS = [
     App.config("ID_SERVER_FREEDOM"),
 ]
 
+MAX_LENGTH = 1920
+MAX_CHARS_UNTIL_THREAD = 364
 TOKEN_COUNT_UNSET = -1
 
 
@@ -55,41 +52,18 @@ class Chat(CustomCog):
         super().__init__()
         self.bot = bot
 
-        self.guild_prompt_history = {}
-        self.guild_prompt_token_count = defaultdict(int)
+        self.chat_history = {}
+        self.token_count = defaultdict(list)
         self.guild_cooldown = defaultdict(dict)
 
         self.chat_model = "gpt-3.5-turbo"
-        self.model_max_output_tokens = 364
-        self.model_temperature = 0.5
-        self.model_max_history_tokens = 1456  # tokens
-        self.model_max_history_remove_fraction = 0.5
+        self.max_output_tokens = 364
+        self.model_temperature = 0.7
+        self.max_tokens_allowed = 1456
+        self.trim_faction = 0.25
+        self.max_chat_history = 20
 
     # Static -------------------------------------------------------------------
-
-    @staticmethod
-    async def __check_if_in_thread(message: disnake.Message) -> bool:
-        """Check if the message came from a thread the bot is in.
-
-        Parameters
-        ----------
-        message : disnake.Message
-            The message to check if in a thread.
-
-        Returns
-        -------
-        bool
-            Returns True if the message came from a thread the bot is in.
-        """
-        in_thread = False
-
-        if isinstance(message.channel, disnake.Thread):
-            thread_members = await message.channel.fetch_members()
-            thread_members = [member.id for member in thread_members]
-            if App.config("ID_BOT") in thread_members:
-                in_thread = True
-
-        return in_thread
 
     @staticmethod
     def __get_cooldown_length(guild_id: int, user: disnake.User | disnake.Member) -> Tuple[int, int]:
@@ -123,6 +97,9 @@ class Chat(CustomCog):
     def __get_history_id(message: disnake.Message) -> int:
         """Determine the history ID to use given the origin of the message.
 
+        Will either be the user id for direct messages, or the id for the
+        channel.
+
         Parameters
         ----------
         message : disnake.Message
@@ -135,10 +112,7 @@ class Chat(CustomCog):
         if isinstance(message.channel, disnake.channel.DMChannel):
             history_id = message.author.id
         else:
-            if message.thread and message.thread.owner_id == App.config("ID_BOT"):
-                history_id = message.thread.id
-            else:
-                history_id = message.guild.id
+            history_id = message.channel.id
 
         return history_id
 
@@ -151,9 +125,10 @@ class Chat(CustomCog):
         message :
             The message to respond to.
         """
+        await message.channel.send(f"Stop abusing me " f"{message.author.mention}!", delete_after=10)
+
         try:
             await message.delete(delay=10)
-            await message.channel.send(f"Stop abusing me " f"{message.author.mention}!", delete_after=10)
         except disnake.Forbidden:
             logger.error(f"Bot does not have permission to delete messages in {message.guild.id}")
 
@@ -170,21 +145,19 @@ class Chat(CustomCog):
         response : str
             The response from OpenAI.
         """
+        # can't create threads in DMs or Threads
         if isinstance(message.channel, disnake.channel.DMChannel):
             return message.channel
         if isinstance(message.channel, disnake.Thread):
             return message.channel
 
-        # num_sentences = len(nltk.sent_tokenize(response))
-        num_chars = len(response)
-
-        if num_chars > 364:
+        # but we can create threads in channels, unless we don't have permission
+        if len(response) > MAX_CHARS_UNTIL_THREAD:
             try:
                 message_destination = await message.create_thread(name=f"{response[:20]}...", auto_archive_duration=60)
-                await message_destination.join()
             except disnake.Forbidden:
-                logger.error("Forbidden from creating a thread in channel %d", message.channel.id)
                 message_destination = message.channel
+                logger.error("Forbidden from creating a thread in channel %d", message.channel.id)
         else:
             message_destination = message.channel
 
@@ -192,12 +165,12 @@ class Chat(CustomCog):
 
     # Functions ----------------------------------------------------------------
 
-    def __openai_chat_completion(self, history_id: int) -> str:
+    def __openai_chat_completion(self, history_id: int | str) -> str:
         """Get a message from ChatGPT using the ChatCompletion API.
 
         Parameters
         ----------
-        history_id : int
+        history_id : int | str
             The ID to store chat history context to. Usually the guild or user
             id.
 
@@ -208,23 +181,18 @@ class Chat(CustomCog):
         """
         response = openai.ChatCompletion.create(
             model=self.chat_model,
-            messages=self.guild_prompt_history[history_id],
+            messages=self.chat_history[history_id],
             temperature=self.model_temperature,
-            max_tokens=self.model_max_output_tokens,
+            max_tokens=self.max_output_tokens,
         )
 
-        usage = response["usage"]
         message = response["choices"][0]["message"]["content"]
-
-        # if len(message) > 1920:
-        #     return "I've generated a sentence which is too large for Discord!"
-
-        self.guild_prompt_history[history_id].append({"role": "assistant", "content": message})
-        self.guild_prompt_token_count[history_id] = int(usage["total_tokens"])
+        self.chat_history[history_id].append({"role": "assistant", "content": message})
+        self.token_count[history_id].append(int(response["usage"]["total_tokens"]))
 
         return message
 
-    async def __trim_message_history(self, history_id: int) -> None:
+    async def __trim_message_history(self, history_id: int | str) -> None:
         """Remove messages from a chat history.
 
         Removes a fraction of the messages from the chat history if the number
@@ -233,22 +201,24 @@ class Chat(CustomCog):
 
         Parameters
         ----------
-        history_id : int
+        history_id : int | str
             The chat history ID. Usually the guild or user id.
         """
-        if self.guild_prompt_token_count[history_id] < self.model_max_history_tokens:
-            return
+        if len(self.chat_history[history_id][1:]) > self.max_chat_history:
+            self.chat_history[history_id].pop(1)
 
-        num_remove = int(self.model_max_history_remove_fraction * len(self.guild_prompt_history[history_id]))
-        # num_remove = 1
-        logger.info("Removing last %d messages from %d prompt history", num_remove, history_id)
+        if self.token_count[history_id][-1] > self.max_tokens_allowed:
+            num_remove = int(self.trim_faction * len(self.chat_history[history_id]))
 
-        for i in range(num_remove):
-            if i + 1 > len(self.guild_prompt_history[history_id]) - 2:  # -2 because we exclude the system message
-                break
-            self.guild_prompt_history[history_id].pop(i + 1)  # + 1 to ignore system message
+            tokens_removed = 0
+            for i in range(1, num_remove + 1):
+                if i > len(self.chat_history[history_id]) - 2:  # -2 because we exclude the system message
+                    break
+                self.chat_history[history_id].pop(i)
+                tokens_removed += self.token_count[history_id].pop(i)
 
-        self.guild_prompt_token_count[history_id] = TOKEN_COUNT_UNSET
+            for i in range(1, len(self.chat_history[history_id])):
+                self.token_count[history_id][i] -= tokens_removed
 
     async def __check_for_cooldown(self, message: disnake.Message) -> bool:
         """Check if a message author is on cooldown.
@@ -263,6 +233,9 @@ class Chat(CustomCog):
         bool
             True if the use is on cooldown, False if not.
         """
+        if isinstance(message.channel, disnake.DMChannel):
+            return False
+
         current_time = time.time()
         last_message_time, message_count = self.guild_cooldown[message.guild.id].get(message.author.id, (0, 0))
         elapsed_time = current_time - last_message_time
@@ -273,14 +246,13 @@ class Chat(CustomCog):
 
         if message_count >= cooldown_length:
             message_count = 0
-
         message_count += 1
 
         self.guild_cooldown[message.guild.id][message.author.id] = (current_time, message_count)
 
         return False
 
-    async def respond_to_prompt(self, history_id: int, prompt: str) -> str:
+    async def respond_to_prompt(self, history_id: int | str, prompt: str) -> str:
         """Process a prompt and get a response.
 
         This function is the main steering function for getting a response from
@@ -302,14 +274,14 @@ class Chat(CustomCog):
         str
             The generated response to the given prompt.
         """
-        prompt = prompt.replace("@Margaret", "", 1).strip()  # todo, remove hardcoded reference
+        prompt = prompt.replace("@Margaret", "", 1).strip()
 
-        if history_id not in self.guild_prompt_history:
-            self.guild_prompt_token_count[history_id] = 0
-            self.guild_prompt_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
+        if history_id not in self.chat_history:
+            self.token_count[history_id] = [0]
+            self.chat_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
         await self.__trim_message_history(history_id)
-        self.guild_prompt_history[history_id].append({"role": "user", "content": prompt})
+        self.chat_history[history_id].append({"role": "user", "content": prompt})
 
         try:
             response = self.__openai_chat_completion(history_id)
@@ -340,9 +312,8 @@ class Chat(CustomCog):
         # only respond when mentioned, in DMs or when in own thread
         bot_mentioned = App.config("BOT_USER_OBJECT") in message.mentions
         message_in_dm = isinstance(message.channel, disnake.channel.DMChannel)
-        in_thread = await self.__check_if_in_thread(message)
 
-        if bot_mentioned or message_in_dm or in_thread:
+        if bot_mentioned or message_in_dm:
             history_id = self.__get_history_id(message)
 
             if not message_in_dm and message.guild.id in TIME_LIMITED_SERVERS:
@@ -362,7 +333,7 @@ class Chat(CustomCog):
     # Commands -----------------------------------------------------------------
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(name="clear_ai_chat_history", description="reset your AI chat history")
+    @commands.slash_command(name="clear_chat_history", description="reset the AI chat history")
     async def clear_chat_history(self, inter: disnake.ApplicationCommandInteraction) -> coroutine:
         """Clear history context for where the interaction was called from.
 
@@ -372,18 +343,16 @@ class Chat(CustomCog):
             The slash command interaction.
         """
         history_id = inter.guild.id if inter.guild else inter.author.id
-
-        if history_id not in self.guild_prompt_history:
+        if history_id not in self.chat_history:
             return await inter.response.send_message("There is no chat history to clear.", ephemeral=True)
-
-        self.guild_prompt_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
+        self.chat_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
 
         return await inter.response.send_message(
             "System prompt reset to default and chat history cleared.", ephemeral=True
         )
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(name="set_new_system_prompt", description="change the chat system prompt")
+    @commands.slash_command(name="set_system_prompt", description="change the chat system prompt")
     async def set_system_message(self, inter: disnake.ApplicationCommandInteraction, message: str) -> coroutine:
         """Set a new system message for the location were the interaction came
         from.
@@ -399,31 +368,18 @@ class Chat(CustomCog):
             The new system prompt to set.
         """
         history_id = inter.guild.id if inter.guild else inter.author.id
-        self.guild_prompt_history[history_id] = [{"role": "system", "content": message}]
+        self.chat_history[history_id] = [{"role": "system", "content": message}]
 
         return await inter.response.send_message(
             "System prompt updated and chat history cleared.",
             ephemeral=True,
         )
 
-    @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(name="reset_chat_history", description="reset the chat history")
-    async def reset_chat_history(self, inter: disnake.ApplicationCommandInteraction) -> coroutine:
-        """Reset the chat history to default.
-
-        Parameters
-        ----------
-        inter : disnake.ApplicationCommandInteraction
-            The slash command interaction.
-        """
-        history_id = inter.guild.id if inter.guild else inter.author.id
-        self.guild_prompt_history[history_id] = [{"role": "system", "content": DEFAULT_SYSTEM_MESSAGE}]
-
-        return await inter.response.send_message("Chat history reset.", ephemeral=True)
+    # Admin commands -----------------------------------------------------------
 
     @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
     @commands.slash_command(
-        name="set_chat_output_tokens", description="change the maximum number of output tokens for a ai response"
+        name="set_output_tokens", description="change the maximum number of output tokens for an ai response"
     )
     @commands.default_member_permissions(administrator=True)
     async def set_max_tokens(
@@ -438,35 +394,14 @@ class Chat(CustomCog):
         num_tokens : int
             The number of tokens
         """
-        self.model_max_output_tokens = num_tokens
-        self.model_max_history_tokens = num_tokens * 3
+        self.max_output_tokens = num_tokens
+        self.max_tokens_allowed = num_tokens * 3
 
-        if self.model_max_history_tokens > 2048:
-            self.model_max_history_tokens = 2048
+        if self.max_tokens_allowed > 2048:
+            self.max_tokens_allowed = 2048
+        if self.max_tokens_allowed < 768:
+            self.max_tokens_allowed = 768
 
-        logger.info("Output tokens set to %d", num_tokens)
-
-        await inter.response.send_message(f"Max output tokens set to {num_tokens}", ephemeral=True)
-
-    @commands.cooldown(App.config("COOLDOWN_RATE"), App.config("COOLDOWN_STANDARD"), COOLDOWN_USER)
-    @commands.slash_command(
-        name="set_model_temperature",
-        description="change the temperature of the model which affects the randomness of responses",
-    )
-    @commands.default_member_permissions(administrator=True)
-    async def set_model_temperature(
-        self, inter: disnake.ApplicationCommandInteraction, temperature: float = commands.Param(ge=0, le=2)
-    ) -> coroutine:
-        """Set the number of tokens the model can return.
-
-        Parameters
-        ----------
-        inter : disnake.Interaction
-            The slash command interaction.
-        temperature: float
-            The new temperature to use
-        """
-        self.model_temperature = temperature
-        logger.info("Model temperature tokens set to %f", temperature)
-
-        await inter.response.send_message(f"Model temperature set to {temperature}", ephemeral=True)
+        return await inter.response.send_message(
+            f"Max output tokens set to {num_tokens} with a token total of {self.max_tokens_allowed}.", ephemeral=True
+        )
