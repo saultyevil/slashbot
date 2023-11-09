8fdb741afcc0cc384ba0805af20ce3711f3a7db3
Edward J. Parkinson <saultyevil@gmail.com>
2023-11-09 21:12:05 +0000

    apparently footers dont support markdown

--- slashbot/cogs/weather.py
+++ slashbot/cogs/weather.py
@@ -273,7 +273,7 @@ class Weather(SlashbotCog):
             )
 
         embed.set_footer(
-            text=f"{await self.get_generated_sentence('forecast')}\n*(You can set your location using /set_info)*"
+            text=f"{await self.get_generated_sentence('forecast')}\n(You can set your location using /set_info)"
         )
         embed.set_thumbnail(self.__get_weather_icon_url(forecast[0]["weather"][0]["icon"]))
 
@@ -369,7 +369,7 @@ class Weather(SlashbotCog):
         )
 
         embed.set_footer(
-            text=f"{await self.get_generated_sentence('weather')}\n*(You can set your location using /set_info)*"
+            text=f"{await self.get_generated_sentence('weather')}\n(You can set your location using /set_info)"
         )
         embed.set_thumbnail(self.__get_weather_icon_url(current_weather["weather"][0]["icon"]))
 
