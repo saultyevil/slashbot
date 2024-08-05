10fea3db2478c32b41886a274891b7484ede016c
Edward J. Parkinson <saultyevil@gmail.com>
2024-08-05 14:53:51 +0100

    re-add macbot-like markov prompt and improve error reporting for cog markov generation

--- bot/custom_cog.py
+++ bot/custom_cog.py
@@ -31,7 +31,7 @@ class SlashbotCog(commands.Cog):
         """
         super().__init__()
         self.bot = bot
-        self.markov_sentences = {}
+        self.premade_markov_sentences = {}
         self.regenerate_markov_sentences.start()
 
     # Before command invokes ---------------------------------------------------
@@ -60,12 +60,12 @@ class SlashbotCog(commands.Cog):
     @tasks.loop(seconds=30)
     async def regenerate_markov_sentences(self) -> None:
         """Re-generate the markov sentences with a given seed word."""
-        if not self.bot.markov_gen_enabled or not self.markov_sentences:
+        if not self.bot.markov_gen_enabled or not self.premade_markov_sentences:
             return
 
-        for seed_word, seed_sentences in self.markov_sentences.items():
+        for seed_word, seed_sentences in self.premade_markov_sentences.items():
             if len(seed_sentences) <= App.get_config("PREGEN_REGENERATE_LIMIT"):
-                self.markov_sentences[seed_word] = await async_generate_list_of_sentences_with_seed_word(
+                self.premade_markov_sentences[seed_word] = await async_generate_list_of_sentences_with_seed_word(
                     MARKOV_MODEL,
                     seed_word,
                     App.get_config("PREGEN_MARKOV_SENTENCES_AMOUNT"),
@@ -95,16 +95,16 @@ class SlashbotCog(commands.Cog):
             The generated sentence.
 
         """
-        if seed_word not in self.markov_sentences:
+        if seed_word not in self.premade_markov_sentences:
             if self.bot.markov_gen_enabled:
-                logger.error("No pre-generated markov sentences for seed word %s ", seed_word)
+                logger.error("Seed word '%s' is missing in pre-made markov sentences", seed_word)
             return generate_markov_sentence(MARKOV_MODEL, seed_word)
 
         try:
-            return self.markov_sentences[seed_word].pop()
+            return self.premade_markov_sentences[seed_word].pop()
         except IndexError:
             if self.bot.markov_gen_enabled:
-                logger.debug("Using generate_sentence instead of pre gen sentences for %s", seed_word)
+                logger.exception("Unable to get pre-made markov sentence for seed word '%s'", seed_word)
             return generate_markov_sentence(MARKOV_MODEL, seed_word)
 
     async def async_get_markov_sentence(self, seed_word: str) -> str:
@@ -124,14 +124,14 @@ class SlashbotCog(commands.Cog):
             The generated sentence.
 
         """
-        if seed_word not in self.markov_sentences:
+        if seed_word not in self.premade_markov_sentences:
             if self.bot.markov_gen_enabled:
-                logger.error("No pre-generated markov sentences for seed word %s ", seed_word)
+                logger.error("Seed word '%s' is missing in pre-made markov sentences", seed_word)
             return await async_generate_sentence(MARKOV_MODEL, seed_word)
 
         try:
-            return self.markov_sentences[seed_word].pop()
+            return self.premade_markov_sentences[seed_word].pop()
         except IndexError:
             if self.bot.markov_gen_enabled:
-                logger.debug("Using generate_sentence instead of pre gen sentences for %s", seed_word)
+                logger.exception("Unable to get pre-made markov sentence for seed word '%s'", seed_word)
             return await async_generate_sentence(MARKOV_MODEL, seed_word)
