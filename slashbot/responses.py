4ade0a33fd2eaac99437d39683cfcab3ed30ab15
Edward J. Parkinson <saultyevil@gmail.com>
2025-03-08 18:14:11 +0000

    missed file

--- slashbot/responses.py
+++ slashbot/responses.py
@@ -1,4 +1,4 @@
-from slashbot.types import InteractionReference, Message
+from slashlib.types import InteractionReference, Message
 
 
 async def is_reply_to_slash_command_response(message: Message) -> bool:
