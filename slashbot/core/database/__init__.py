184f7de2645d513a5515c9f46b3cc836b5ade6be
Edward J. Parkinson <saultyevil@gmail.com>
2025-07-18 20:26:09 +0100

    update database module

--- slashbot/core/database/__init__.py
+++ slashbot/core/database/__init__.py
@@ -1,10 +1,15 @@
 """Asynchronous database module, using a JSON backend as key-value store."""
 
 from .kv import DatabaseKV
-from .models import Reminder, User
+from .models import Reminder, User, WikiFeetModel, WikiFeetPicture
+from .wikifeet import WikiFeetDatabase, WikiFeetScraper
 
 __all__ = [
     "DatabaseKV",
     "Reminder",
     "User",
+    "WikiFeetDatabase",
+    "WikiFeetModel",
+    "WikiFeetPicture",
+    "WikiFeetScraper",
 ]
