6fe965bc96ca983675f42dea3d1f1cf345108f0f
Edward J. Parkinson <saultyevil@gmail.com>
2025-08-25 13:05:55 +0100

    add script to migrate between kv and sql db

---
+++ scripts/migrate_database_from_kv_to_sql.py
@@ -0,0 +1,44 @@
+import asyncio
+import datetime
+from pathlib import Path
+
+from slashbot.core.database import Database, DatabaseKV, Reminder, ReminderKVModel, User, UserKVModel
+from slashbot.core.logger import setup_logging
+from slashbot.settings import BotSettings
+
+setup_logging()
+
+
+async def main():
+    database_kv_path = Path("data/slashbot.db.json").absolute()
+    database_kv = await DatabaseKV.open(filepath=database_kv_path)
+    database_sql = Database(BotSettings.files.database)
+    await database_sql.init()
+
+    users_kv = await database_kv.get_users()
+    for user in users_kv:
+        await database_sql.add_user(
+            User(
+                discord_id=user.user_id,
+                username=user.user_name,
+                city=user.city,
+                country_code=user.country_code,
+                bad_word=user.bad_word,
+            )
+        )
+
+    reminders_kv = await database_kv.get_reminders()
+    for reminder in reminders_kv:
+        await database_sql.add_reminder(
+            Reminder(
+                user_id=reminder.user_id,
+                channel_id=reminder.channel_id,
+                date=datetime.datetime.fromisoformat(reminder.date_iso),
+                content=reminder.content,
+                tagged_users=reminder.tagged_users,
+            )
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
