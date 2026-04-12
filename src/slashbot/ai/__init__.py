52788bc325c5b2d44c0ac6b2dfc34e2b0c06f461
Edward J. Parkinson <saultyevil@gmail.com>
2026-04-12 15:11:36 +0100

    remove cog specific code from ai module

--- src/slashbot/ai/__init__.py
+++ src/slashbot/ai/__init__.py
@@ -1,9 +1,7 @@
 """Core AI module for Slashbot."""
 
-from .chat import AIChat
 from .models import GenerationFailureError, TextGenerationInput, TextGenerationResponse, VisionImage, VisionVideo
 from .prompts import Prompt, read_in_prompt
-from .summary import AIChatSummary, SummaryMessage
 from .text_generator import TextGenerator
 
 SUPPORTED_MODELS = TextGenerator.SUPPORTED_MODELS
@@ -11,11 +9,8 @@ SUPPORTED_MODELS = TextGenerator.SUPPORTED_MODELS
 
 __all__ = [
     "SUPPORTED_MODELS",
-    "AIChat",
-    "AIChatSummary",
     "GenerationFailureError",
     "Prompt",
-    "SummaryMessage",
     "TextGenerationInput",
     "TextGenerationResponse",
     "TextGenerator",
