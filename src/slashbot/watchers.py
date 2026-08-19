72c15ac1e5c4f647ae308bd6ea340863fceaa7d4
Edward J. Parkinson <saultyevil@gmail.com>
2026-08-19 17:19:13 +0100

    remove llm_old

--- src/slashbot/watchers.py
+++ src/slashbot/watchers.py
@@ -1,41 +1,8 @@
 import time
 
-import pydantic
-import yaml
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
-from watchdog.observers import Observer
 
-from slashbot.llm_old.prompts import create_prompt_dict, read_in_prompt
-from slashbot.logger import Logger
 from slashbot.settings import BotSettings
-
-AVAILABLE_LLM_PROMPTS = create_prompt_dict()
-LOGGER = Logger()
-
-
-class PromptFileWatcher(FileSystemEventHandler):
-    """File watched for prompt files for LLM purposes."""
-
-    def on_any_event(self, event: FileSystemEvent) -> None:
-        """Handle any file system event.
-
-        This method is called when any file system event occurs.
-        It updates the `PROMPT_CHOICES` dictionary based on the event type and
-        source path.
-        """
-        global AVAILABLE_LLM_PROMPTS  # noqa: PLW0603
-        if event.is_directory and not event.src_path.endswith(".yaml"):
-            return
-        try:
-            if event.event_type in ["created", "modified"]:
-                prompt = read_in_prompt(event.src_path)
-                AVAILABLE_LLM_PROMPTS[prompt.name] = prompt.prompt
-                LOGGER.log_debug("%s prompt %s", event.event_type.capitalize(), event.src_path)
-            if event.event_type == "deleted":
-                AVAILABLE_LLM_PROMPTS = create_prompt_dict()
-                LOGGER.log_debug("%s prompt %s", event.event_type.capitalize(), event.src_path)
-        except (yaml.YAMLError, pydantic.ValidationError):
-            LOGGER.log_exception("Error reading in prompt file %s", event.src_path)
 
 
 class ScheduledPostWatcher(FileSystemEventHandler):
@@ -71,8 +38,3 @@ class ScheduledPostWatcher(FileSystemEventHandler):
                 while self.parent.post_loop.is_running():
                     time.sleep(0.5)
             self.parent.post_loop.start()
-
-
-FILE_OBSERVER = Observer()
-FILE_OBSERVER.schedule(PromptFileWatcher(), "data/prompts", recursive=True)
-FILE_OBSERVER.start()
